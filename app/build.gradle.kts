f3d0a4ba34af8749b44e8bb06863529e4e34075c
alejandroestevemaza <alex.esteve98@gmail.com>
2026-04-16 22:28:20 +0200

    v.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.alejandroestevemaza.gymtimerpro"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
